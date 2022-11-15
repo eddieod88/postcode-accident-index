d4d32e095538184f9a5881619e78c0a640e11a62
Eddie O'Donnell <eddieod88@gmail.com>
2022-11-15 15:25:56 +0000

    cleaning and creation of geodataframe

--- scripts/make_group_estimator_data.py
+++ scripts/make_group_estimator_data.py
@@ -1,35 +1,85 @@
 import asyncio
 from pathlib import Path
 
+import geopandas
+from loguru import logger
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 from src.utils.log import log_step
-from src.api.clients.postcodes_io import PostcodeClient
+from src.api.clients.postcodes_io import PostcodeClient, PostcodeField, PostcodePayload
 
 
 ROOT = Path(__file__).parents[1]
 
+ISLANDS = [
+    "IM",  # Isle of Man
+    "GY",  # Guernsey
+]
+
 
 @log_step
-def clean(df: pd.DataFrame):
+def clean(df: pd.DataFrame) -> pd.DataFrame:
     return df
+
+
+@log_step
+def add_postcode_element_columns(df: pd.DataFrame) -> pd.DataFrame:
+    postcode_regex = r"^[A-Z]{1,2}[0-9][A-Z0-9]? ?[0-9][A-Z]{2}$"
+    df["is_ok"] = df["postcode"].str.match(postcode_regex)
+    df["area"] = df["postcode"].str.extract(r"(?P<area>^[A-Z]{1,2})[0-9][A-Z0-9]? ?[0-9][A-Z]{2}$")
+    df["district"] = df["postcode"].str.extract(
+        r"(?P<district>^[A-Z]{1,2}[0-9][A-Z0-9]?) ?[0-9][A-Z]{2}$"
+    )
+    df["sector"] = df["postcode"].str.extract(
+        r"(?P<sector>^[A-Z]{1,2}[0-9][A-Z0-9]? ?[0-9])[A-Z]{2}$"
+    )
+    return df
+
+
+@log_step
+def remove_islands(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Remove postcodes which are in islands off the mainland - we do not have data for this.
+    """
+    df = df[~df["area"].isin(ISLANDS)].copy()
+    return df
+
+
+@log_step
+def remove_nan_premiums(df: pd.DataFrame) -> pd.DataFrame:
+    return df[df["avgprice1_5"].notna()].copy()
 
 
 # change to polars maybe??
 @log_step
-def add_locations(df: pd.DataFrame, df_locations: pd.DataFrame):
+def add_locations(df: pd.DataFrame, df_locations: pd.DataFrame, override=False) -> pd.DataFrame:
     """
     Add location information to the postcodes, ie longitudes and latitudes
     """
     # TODO Separate this step into two different steps - one updates the database, another updates the dataset
-    
+
+    if override:
+        df_locations = df_locations.head(0).copy()
+
     # find postcodes that we need fetch locations for
     postcodes_fetch_loc = set(df["postcode"]).difference(set(df_locations.index))
+    postcode_api_fields = PostcodePayload(
+        fields=[PostcodeField.LONG, PostcodeField.LAT, PostcodeField.ITL_CODE]
+    )
 
-    if new_locations := asyncio.run(PostcodeClient().fetch_locations(postcodes_fetch_loc)):
+    if new_locations := asyncio.run(
+        PostcodeClient().fetch_locations(postcodes_fetch_loc, postcode_api_fields)
+    ):
         df_new_locations = (
             pd.DataFrame(new_locations)
-            .rename(columns={"longitude": "long", "latitude": "lat"})
+            .rename(
+                columns={
+                    str(PostcodeField.LONG.value): "long",
+                    str(PostcodeField.LAT.value): "lat",
+                    str(PostcodeField.ITL_CODE.value): "itl",
+                },
+            )
             .set_index("postcode")
         )
         df_new_locations.index = df_new_locations.index.str.replace(" ", "")
@@ -37,18 +87,48 @@ def add_locations(df: pd.DataFrame, df_locations: pd.DataFrame):
         df_locations.to_parquet(ROOT / "data/database/locations.parquet")  # horrible
 
     return pd.merge(
-        df, df_locations, how="left", left_on="postcode", right_index=True, validate="m:1"
+        df,
+        df_locations,
+        how="left",
+        left_on="postcode",
+        right_index=True,
+        validate="m:1",
     )
+
+
+@log_step
+def make_geodataframe(df: pd.DataFrame) -> geopandas.GeoDataFrame:
+    """
+    Make a geodataframe with longitudes and latitudes, setting the coord reference system too.
+    """
+    df["coords"] = geopandas.GeoSeries.from_wkt(
+        df.apply(lambda x: f"POINT({x.long} {x.lat})", axis=1)
+    )
+    return geopandas.GeoDataFrame(df, geometry="coords", crs="EPSG:4326")
+
+
+@log_step
+def save(df: pd.DataFrame) -> pd.DataFrame:
+    df.to_parquet(ROOT / "data/processed/df_joined_small.parquet")
+    return df
 
 
 if __name__ == "__main__":
-    columns = ["postcode", "old_postcode_group", "postcode_group"]
+    columns = ["postcode", "postcode_group", "avgprice1_5"]
     df_locations = pd.read_parquet(ROOT / "data/database/locations.parquet")
     df = (
         pd.read_parquet(ROOT / "data/raw/preprocessed_copy_small.parquet", columns=columns)
-        # .sample(1000, random_state=1)
         .pipe(clean)
-        .pipe(add_locations, df_locations)
+        .pipe(add_postcode_element_columns)
+        .pipe(remove_islands)
+        .pipe(remove_nan_premiums)
+        .pipe(add_locations, df_locations, override=True)
+        .pipe(make_geodataframe)
+        .pipe(save)
     )
-    df.to_parquet(ROOT / "data/processed/df_joined_small.parquet")
-    # TODO create districts - need this method for prediction too so make a utility method.
+
+    logger.info("train validation split")
+    df_modelling = df[["long", "lat", "avgprice1_5", "postcode_group"]].copy()
+    df_train, df_validation = train_test_split(df_modelling, test_size=0.2, random_state=1)
+    df_train.to_parquet(ROOT / "data/modelling/df_train.parquet")
+    df_validation.to_parquet(ROOT / "data/modelling/df_validation.parquet")
